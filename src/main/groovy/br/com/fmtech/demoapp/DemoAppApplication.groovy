581ae0e8092281470249314c5d68d108d4658104
Felipe Lima <felipe.lima@unimednatal.com.br>
2021-07-25 16:00:56 -0300

    init: commit inicial

---
+++ src/main/groovy/br/com/fmtech/demoapp/DemoAppApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.fmtech.demoapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication, args)
+	}
+
+}
